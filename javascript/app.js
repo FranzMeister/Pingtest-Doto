2794d862e0b0729d9474c2e4dc8979384094472d
Vladislav Supalov <vladislav@supalov.com>
2014-04-09 13:50:08 +0200

    Some basic routing, renaming of things.

--- javascript/app.js
+++ javascript/app.js
@@ -1,11 +1,22 @@
-App = Ember.Application.create();
-
-App.Router.map(function() {
-  // put your routes here
+App = Ember.Application.create({
+    LOG_TRANSITIONS: true
 });
 
-App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return [];
-  }
+App.HomeController = Em.Controller.extend();
+App.HomeView = Em.View.extend({
+    templateName: 'home'
+});
+
+App.ImprintController = Em.Controller.extend();
+App.ImprintView = Em.View.extend({
+    templateName: 'imprint'
+});
+
+App.Router.map(function() {
+    this.route("home", { path: "/" });
+    this.route("imprint", { path: "/imprint" });
+});
+
+App.Router.reopen({
+  rootURL: '/'
 });
